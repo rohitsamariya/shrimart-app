65b3d2f54db2c2ae2b47382f1546b9788126ad0c
rohitsamariya01 <rohitsocial2023@gmail.com>
2026-03-18 16:51:30 +0530

    Add ErrorBoundary to catch and display React runtime errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,33 @@
 
+  import React from "react";
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+  class GlobalErrorBoundary extends React.Component<{children: React.ReactNode}, {hasError: boolean, error: any}> {
+    constructor(props: any) {
+      super(props);
+      this.state = { hasError: false, error: null };
+    }
+    static getDerivedStateFromError(error: any) {
+      return { hasError: true, error };
+    }
+    render() {
+      if (this.state.hasError) {
+        return (
+          <div style={{ padding: 20, color: 'red', fontFamily: 'monospace', wordBreak: 'break-all' }}>
+            <h1>Something went wrong.</h1>
+            <pre>{this.state.error?.toString()}</pre>
+            <pre>{this.state.error?.stack}</pre>
+          </div>
+        );
+      }
+      return this.props.children;
+    }
+  }
+
+  createRoot(document.getElementById("root")!).render(
+    <GlobalErrorBoundary>
+      <App />
+    </GlobalErrorBoundary>
+  );
